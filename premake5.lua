2524ca28e28888801502ca841a0a543ec13f1d05
timeester3648 <weteringt@gmail.com>
2024-03-07 17:34:23 +0100

    Update

---
+++ premake5.lua
@@ -0,0 +1,126 @@
+include "../../premake/common_premake_defines.lua"
+
+project "OpenSSL"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "%{prj.name}"
+	inlining "Auto"
+
+	includedirs {
+		"%{IncludeDir.zlib}",
+		"%{IncludeDir.openssl}",
+
+		".",
+		"./crypto",
+		"./crypto/",
+		"./crypto/modes",
+		"./crypto/include",
+		"./crypto/ec/curve448",
+		"./providers/common/include",
+		"./crypto/ec/curve448/arch_32",
+		"./providers/implementations/include"
+	}
+
+	files {
+		"./ssl/**.h",
+		"./ssl/**.c",
+		"./crypto/**.h",
+		"./crypto/**.c",
+
+		"./engines/e_capi.c",
+		"./engines/e_padlock.c",
+
+		"./providers/*.h",
+		"./providers/baseprov.c",
+		"./providers/nullprov.c",
+		"./providers/defltprov.c",
+		"./providers/prov_running.c",
+		"./providers/implementations/asymciphers/rsa_enc.c",
+
+		"./**.h.in",
+		"./**x86_64.pl",
+		"./util/mkbuildinf.pl"
+	}
+
+	excludes {
+		"./crypto/ec/ecp_nistz256_table.c",
+		"./crypto/rsa/rsa_acvp_test_params.c",
+
+		"./crypto/md2/**",
+		"./crypto/rc5/**",
+	}
+
+	filter "toolset:msc"
+		disablewarnings { "4311", "4996", "4267", "4244", "4305", "4013", "4133", "4334" }
+
+	filter "system:windows"
+		defines {
+			"WIN32_LEAN_AND_MEAN",
+			"OPENSSL_PIC",
+			"L_ENDIAN",
+			"OPENSSL_SYS_WIN32",
+			"OPENSSL_BUILDING_OPENSSL",
+
+			"ENGINESDIR=\".\"",
+			"OPENSSLDIR=\".\"",
+			"MODULESDIR=\".\""
+		}
+
+		excludes {
+			"./crypto/s390xcap.c",
+			"./crypto/sparcv9cap.c",
+			"./crypto/loongarchcap.c",
+			"./crypto/ec/ecp_nistp256.c",
+			"./crypto/ec/ecp_nistp224.c",
+			"./crypto/ec/ecp_nistp521.c",
+			"./crypto/ec/ecp_nistp384.c",
+			"./crypto/poly1305/poly1305_ieee754.c",
+			"./crypto/poly1305/poly1305_base2_44.c",
+
+			"**LPdir**",
+
+			"./crypto/**_vms.c",
+			"./crypto/**_unix.c",
+
+			
+			"./ssl/ktls_**.c",
+		}
+
+		files {
+			"./ms/uplink.c"
+		}
+
+	filter { "system:windows", "files:**mkbuildinf.pl" }
+		buildmessage "Generating build info"
+
+		buildcommands {
+			"perl \"-I.\" \"-Iutil\\perl\" \"-Iproviders\\common\\der\" \"-Mconfigdata\" \"-MOpenSSL::paramnames\" \"-Moids_to_c\" \"util\\dofile.pl\" \"-omakefile\" \"VC-WIN64A\"> .\\crypto\\buildinf.h"
+		}
+
+		buildoutputs { ".\\crypto\\buildinf.h" }
+
+	-- TODO: also other platforms
+	filter { "system:windows", "files:**.h.in" }
+		buildmessage "Generating %{file.basename}"
+
+		buildcommands {
+			"perl \"-I.\" \"-Iutil\\perl\" \"-Iproviders\\common\\der\" \"-Mconfigdata\" \"-MOpenSSL::paramnames\" \"-Moids_to_c\" \"util\\dofile.pl\" \"-omakefile\" %{file.relpath} > %{file.reldirectory}\\%{file.basename}"
+		}
+
+		buildoutputs { "%{file.reldirectory}\\%{file.basename}" }
+
+	filter { "system:windows", "files:**x86_64.pl" }
+		buildmessage "Compiling %{file.basename}.asm"
+
+		buildcommands {
+			"set ASM=ml64 && perl %{file.relpath} masm %{cfg.objdir}/%{file.basename}.asm && ml64 /c /Cp /Cx /nologo /Zi /Fo%{cfg.objdir}/%{file.basename}.obj %{cfg.objdir}/%{file.basename}.asm"
+		}
+
+		buildoutputs { "%{cfg.objdir}/%{file.basename}.asm", "%{cfg.objdir}/%{file.basename}.obj" }
+
+		
+
+
+	
